73cc20f91a29f16faf334ef2866f192ebc1bec9a
Luka Brnetic <brnetic@usc.edu>
2022-11-24 05:58:30 +0000

    Initial commit for hw5

---
+++ wordle.cpp
@@ -0,0 +1,38 @@
+// For debugging
+#include <iostream>
+// For std::remove
+#include <algorithm> 
+#include <map>
+#include <set>
+#include "wordle.h"
+#include "dict-eng.h"
+using namespace std;
+
+
+// Add prototypes of helper functions here
+
+
+// Definition of primary wordle function
+std::set<std::string> wordle(
+    const std::string& in,
+    const std::string& floating,
+    const std::set<std::string>& dict)
+{
+    // Add your code here
+    std::set<std::string> storage;
+
+    
+        
+
+}
+
+std::string fill(std::string in, int n){
+    if(n = in.size()){
+
+        return;
+    }
+    
+}
+
+
+// Define any helper functions here
