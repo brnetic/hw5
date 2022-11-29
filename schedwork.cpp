e8f60aa5b3b9df71f77fd63f27b28f18128b0755
Luka Brnetic <brnetic@usc.edu>
2022-11-29 07:49:13 +0000

    adding everything

--- schedwork.cpp
+++ schedwork.cpp
@@ -20,7 +20,12 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 // Add prototypes for any helper functions here
 
-
+bool scheduleHelper(const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched, int day);
+bool check(const size_t maxShifts,
+    DailySchedule& sched, int id);
 // Add your implementation of schedule() and other helper functions here
 
 bool schedule(
@@ -36,8 +41,60 @@ bool schedule(
     sched.clear();
     // Add your code below
 
+    for(int i = 0;i<avail.size();i++){
+        sched.push_back(vector<Worker_T>());
+    }
+    return scheduleHelper(avail,dailyNeed,maxShifts,sched,0);
+    
 
 
 
 }
+bool scheduleHelper(const AvailabilityMatrix& avail,
+    const size_t dailyNeed,
+    const size_t maxShifts,
+    DailySchedule& sched, int day)
+{
+    if(day == avail.size()){
+        return true;
+    }
+    if(sched[day].size() >= dailyNeed){
+        return scheduleHelper(avail,dailyNeed,maxShifts,sched,day+1);
+    }
 
+
+    for(int i = 0;i<avail[day].size();i++){
+        if(avail[day][i]){
+            sched[day].push_back(i);
+            if(check(maxShifts,sched,i)){
+                if(scheduleHelper(avail,dailyNeed,maxShifts,sched,day)){
+                    return true;
+                }
+            }
+            sched[day].pop_back();
+
+        }
+    }
+    return false;
+
+
+}
+
+bool check(const size_t maxShifts,
+    DailySchedule& sched, int id)
+{
+    int br = 0;
+    for(int i = 0;i<sched.size();i++){
+			int br2 = 0;
+        for(int j = 0;j<sched[i].size();j++){
+            if(sched[i][j] == id){
+                br++;
+								br2++;
+						}
+        }
+				if(br2>1){
+					return false;
+				}
+    }
+    return br <= maxShifts;
+}
--- wordle.cpp
+++ wordle.cpp
@@ -1,6 +1,7 @@
 // For debugging
 #include <iostream>
 // For std::remove
+#include <string>
 #include <algorithm> 
 #include <map>
 #include <set>
@@ -10,8 +11,8 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
-
+void fillWords(std::string in,const std::set<std::string>& dict, std::set<std::string>& words);
+void fillit(std::string in, std::string floating, const std::set<std::string>& dict, std::set<std::string>& words);
 // Definition of primary wordle function
 std::set<std::string> wordle(
     const std::string& in,
@@ -20,18 +21,48 @@ std::set<std::string> wordle(
 {
     // Add your code here
     std::set<std::string> storage;
-
+		fillit(in,floating,dict,storage);
+		return storage;
+		
     
         
 
 }
 
-std::string fill(std::string in, int n){
-    if(n = in.size()){
+void fillit(std::string in, std::string floating, const std::set<std::string>& dict, std::set<std::string>& words){
+    if(floating.size() == 0){
+			fillWords(in,dict,words);
+			return;
 
-        return;
-    }
-    
+			
+		}
+
+		else{
+			for(int i = 0;i<in.size();i++){
+				if(in[i] == '-'){
+					std::string in2 = in;
+					in2[i] = floating[0];
+					fillit(in2,floating.substr(1,std::string::npos),dict,words);
+				}
+			}
+		}
+		
+		
+}
+void fillWords(std::string in,const std::set<std::string>& dict, std::set<std::string>& words){
+	if(in.find('-') == std::string::npos){
+		if(dict.find(in) != dict.end()){
+			words.insert(in);
+		}
+		return;
+	}
+	else{
+		int i = in.find('-');
+		for(int j = 0;j<26;j++){
+			in[i] = char(j+97);
+			fillWords(in,dict,words);
+		}
+	}
 }
 
 
